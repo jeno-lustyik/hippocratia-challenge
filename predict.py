4ef12f07efd0d83cac2f24f6cb5b40cbc68b013f
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-03-20 22:11:34 +0100

    updated predict and model

--- predict.py
+++ predict.py
@@ -1,9 +1,9 @@
 import joblib
 
-model_joblib = joblib.load('model.pkl')
+model_joblib = joblib.load('new_model.pkl')
 
 while True:
-    age = int(input("How old are you? \n"))
+    age_grps = int(input("How old are you? [0-3: Young, Adult, Old, Senior ] \n"))
     sex = int(input("What is your gender? \n"))
     cp = int(input("Which type of chest pain do you have? [0, 1, 2, 3] \n"))
     trtbps = int(input("What is your resting blood pressure? \n"))
@@ -17,7 +17,7 @@ while True:
     caa = int(input("The number of your major vessels: [0-3] \n"))
     thal = int(input("Thal: [1, 2, 3] \n"))
 
-    x_test = [[age, sex, cp, trtbps, chol, fbs, restecg, thalach, exang, oldpeak, slp, caa, thal]]
+    x_test = [[age_grps, sex, cp, trtbps, chol, fbs, restecg, thalach, exang, oldpeak, slp, caa, thal]]
     prediction = model_joblib.predict(x_test)
     if prediction == 0:
         print("You're safe!")
